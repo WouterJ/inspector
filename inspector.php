a6a33f563182217c956137b5ed57bb4414e01e76
WouterJ <waldio.webdesign@gmail.com>
2013-01-01 12:42:53 +0100

    Changed array to ArrayObject in console.command

--- inspector.php
+++ inspector.php
@@ -7,26 +7,29 @@ require_once __DIR__.'/vendor/autoload.php';
 \*-----------------------------------*/
 $container = new Pimple();
 
+// finder
 $container['finder.class'] = 'Symfony\Component\Finder\Finder';
 $container['finder'] = function ($c) {
     return new $c['finder.class']();
 };
 
+// event dispatcher
 $container['event_dispatcher.class'] = 'Symfony\Component\EventDispatcher\EventDispatcher';
 $container['event_dispatcher'] = $container->share(function ($c) {
     return new $c['event_dispatcher.class']();
 });
 
+// inspector
 $container['inspector.class'] = 'Inspector\Inspector';
 $container['inspector'] = function ($c) {
     return new $c['inspector.class']($c['finder'], $c['event_dispatcher']);
 };
 
-$container['console.command'] = array(
-    'inspector' => function () {
-        return new Inspector\Console\Command\InspectorCommand();
-    },
-);
+// console
+$container['console.command'] = new ArrayObject();
+$container['console.command']['inspector'] = function () {
+    return new Inspector\Console\Command\InspectorCommand();
+};
 
 $container['console.application.class'] = 'Inspector\Console\Application';
 $container['console.application'] = $container->share(function ($c) {
